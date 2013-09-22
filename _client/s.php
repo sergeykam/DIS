00a3ad581df45046c8735b0d3c49401998323407
sergeykam <sergeykam@rambler.ru>
2013-09-22 22:11:18 +0400

    новый индекс хтмл

--- _client/s.php
+++ _client/s.php
@@ -2,7 +2,8 @@
 
 $dis_data = array(rand(0, 3),1,1,3,30, floatval(20 . '.' . rand(0, 50000)));
 $dis_data = array(0,1,1,1,30, rand(0, 8000));
-//$dis_data = array(0,1,1,1,30, 1000);
+$dis_data = array(0,1,1,1,30, 1000);
+$dis_data = array(0,1,1,1,30, '0.0000');
 
 if(!empty($_GET['a']) && isset($_GET['a'])){
 	echo json_encode($dis_data, true);
