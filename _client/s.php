3e449820c1fdf5efbc140411aff93b8f9105e50f
sergeykam <sergeykam@rambler.ru>
2014-01-24 12:44:00 +0400

    Новая версия

--- _client/s.php
+++ _client/s.php
@@ -1,9 +1,9 @@
 <?php
 
-$dis_data = array(rand(0, 3),1,1,3,30, floatval(20 . '.' . rand(0, 50000)));
-$dis_data = array(0,1,1,1,30, rand(0, 8000));
-$dis_data = array(0,1,1,1,30, 1000);
-$dis_data = array(rand(0, 3),1,01,1,30, rand(0, 8000));
+//$dis_data = array(rand(0, 3),1,1,3,30, floatval(20 . '.' . rand(0, 50000)));
+$dis_data = array(0,3,2,1,30, 667);
+//$dis_data = array(0,1,1,1,30, 1000);
+//$dis_data = array(rand(0, 3),1,01,1,30, rand(0, 8000));
 
 if(!empty($_GET['a']) && isset($_GET['a'])){
 	echo json_encode($dis_data, true);
